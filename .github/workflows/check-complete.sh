7f4f8842983e1858671eed2f65601da70927327f
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-08-16 11:22:51 +0530

    Update check-complete.sh

--- .github/workflows/check-complete.sh
+++ .github/workflows/check-complete.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
+# Search for '_enter' in any Markdown files in the current directory
 if grep -q '_enter' *.md; then
-  echo "Replace all text having _enter with your input"
+  echo "Replace all occurrences of '_enter' with your input in Markdown files."
   exit 1
 fi
