6ace5680184c24d89de5ca1643ce44debe0f54cd
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-08-16 16:14:16 +0530

    Update ControllerAlert.cs

--- ControllerAlert.cs
+++ ControllerAlert.cs
@@ -5,6 +5,6 @@ public class ControllerAlert : IAlert
     public void SendAlert(BreachType breachType)
     {
        const ushort header = 0xfeed;
-       Console.WriteLine($"0x{header:X} : {breachType}");
+       Console.WriteLine($"0x{header:x} : {breachType}");
     }
 }
